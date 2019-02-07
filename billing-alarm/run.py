2aedc136fc1ed5e6b794268e5aeba6b625fcf903
adrian83 <adrianbrzoza1983@gmail.com>
2019-02-07 20:27:29 +0100

    stack with billing alarm added

--- billing-alarm/run.py
+++ billing-alarm/run.py
@@ -1,18 +1,35 @@
 # -*- coding: utf-8 -*-
 import yaml
 import io
+import boto3
 
-tags = ""
-params = ""
+
+tags = []
+params = []
+template = ""
 
 with open("tags.yml", 'r') as tags_file:
     tags_dict = yaml.load(tags_file)
-    tags = " ".join(["Key={0},Value={1}".format(item[0], item[1]) for item in tags_dict.items()])
+    tags = [{"Key": item[0], "Value": item[1]} for item in tags_dict.items()]
 
 with open("parameters.yml", 'r') as params_file:
     params_dict = yaml.load(params_file)
-    params = " ".join(["ParameterKey={0},ParameterValue={1}".format(item[0], item[1]) for item in params_dict.items()])
+    params = [{"ParameterKey": item[0], "ParameterValue": str(item[1])} for item in params_dict.items()]
+
+with open('cloudformation.yml', 'r') as cfn_template:
+    template=cfn_template.read()
 
 print(tags)
 print(params)
 
+client = boto3.client('cloudformation')
+
+response = client.create_stack(
+    StackName='BILLING-ALARM',
+    #TemplateURL='cloudformation.yml',
+    TemplateBody=template,
+    TimeoutInMinutes=10,
+    OnFailure='DELETE',
+    Parameters=params,
+    Tags=tags
+)
