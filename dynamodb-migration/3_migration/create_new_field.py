8037ca96f85380043fc8be70f79f6297c4a9ed31
adrian83 <adrianbrzoza1983@gmail.com>
2019-05-06 22:34:41 +0200

    migrations scripts added

---
+++ dynamodb-migration/3_migration/create_new_field.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+import io
+import boto3
+import time
+import sys
+from boto3.dynamodb.conditions import Attr
+
+
+
+def scan_all(table, filter):
+
+    lastEvaluatedKey = {}
+
+    while True:
+
+        scanResp = table.scan(FilterExpression=filter, ExclusiveStartKey=lastEvaluatedKey) if lastEvaluatedKey else table.scan(FilterExpression=filter)
+     
+        items = scanResp["Items"]
+        if not items:
+            print("No more items to process")
+            break
+
+        print("Scan fetched {0} items".format(len(items)))
+
+        for item in items:
+            yield item
+
+        if "LastEvaluatedKey" in scanResp:
+            lastEvaluatedKey = scanResp["LastEvaluatedKey"]
+        else:
+            print("No more items to process")
+            break
+
+    print("End of scanning")
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print("provide dynamodb table name")
+        exit(1)
+
+    dynamoDB = boto3.resource("dynamodb")
+
+    table = dynamoDB.Table(sys.argv[1])
+
+    no_age_condition = Attr('age').not_exists()
+
+    count = 0
+    for item in scan_all(table, no_age_condition):
+        
+        # processing items goes here
+        print(str(item))
+
+        table.update_item(
+            Key={
+                'id': item['id']
+            },
+            UpdateExpression='SET age = :val',
+            ExpressionAttributeValues={
+                ':val': 26
+            }
+        )
+        time.sleep(1)
+        count += 1
+
+    print("Items processed: {0}".format(count))
+
